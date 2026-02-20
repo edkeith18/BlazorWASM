174d27a4527fd1da01aa5c486bdf4027976d7582
Ed Keith <edkeith@msn.com>
2026-02-19 20:16:20 -0800

    Add PortfolioData singleton Add Account class Deserialize JSON into PortfolioData

---
+++ Accounts.cs
@@ -0,0 +1,20 @@
+﻿using BlazorWASM.Pages;
+using System;
+public class PortfolioData
+{
+	public List<Account> Accounts { get; set; } = new();
+	public decimal Year { get; set; } = 0;
+
+	public void AddAccount(string name, decimal balance)
+	{
+        Accounts.Add(new Account { Name = name, Balance = balance, LastUpdated = DateTime.Now });
+    }
+
+}
+
+public class Account
+{
+	public string Name { get; set; } = "";
+	public decimal Balance { get; set; }
+	public DateTime LastUpdated { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -8,4 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Register the Accounts service as Scoped.  Scoped will create a new instance for each user session.
+builder.Services.AddScoped<PortfolioData>();
+
 await builder.Build().RunAsync();
